42c0ea54b542326f5e293350ba35daa1c10dd439
Öner Çiller <onerciller@gmail.com>
2017-12-18 10:16:27 +0300

    status code

--- errors.go
+++ errors.go
@@ -2,26 +2,27 @@ package thor
 
 import "net/http"
 
-var (
-	ErrUnsupportedMediaType        = NewHTTPError(http.StatusUnsupportedMediaType)
-	ErrNotFound                    = NewHTTPError(http.StatusNotFound)
-	ErrUnauthorized                = NewHTTPError(http.StatusUnauthorized)
-	ErrForbidden                   = NewHTTPError(http.StatusForbidden)
-	ErrMethodNotAllowed            = NewHTTPError(http.StatusMethodNotAllowed)
-	ErrStatusRequestEntityTooLarge = NewHTTPError(http.StatusRequestEntityTooLarge)
-)
-
 // HTTPError represents an error that occurred while handling a request.
 type HTTPError struct {
-	Code    int
-	Message interface{}
+	Status  int         `json:"status"`
+	Message interface{} `json:"message"`
 }
 
 // NewHTTPError creates a new HTTPError instance.
-func NewHTTPError(code int, message ...interface{}) *HTTPError {
-	he := &HTTPError{Code: code, Message: http.StatusText(code)}
+func NewHTTPError(status int, message ...interface{}) *HTTPError {
+	he := &HTTPError{Status: status, Message: http.StatusText(status)}
 	if len(message) > 0 {
 		he.Message = message[0]
 	}
 	return he
+}
+
+// Error returns the error message.
+func (e *HTTPError) Error() interface{} {
+	return e.Message
+}
+
+// StatusCode returns the HTTP status code.
+func (e *HTTPError) StatusCode() int {
+	return e.Status
 }
